1480d7c37bb93bcab7ea5dd1eac979079145995e
Ali kamar <aliekamar123@gmail.com>
2025-09-30 01:45:13 +0300

    add app_text_style for consistent text styling and responsive font sizing

---
+++ lib/core/themes/app_text_style.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+// semi bold 600    regular 400  medium 500
+abstract class AppTextStyle {
+  static TextStyle styleBold24(context) {
+    return GoogleFonts.arimo(
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleBold16(context) {
+    return GoogleFonts.arimo(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w700,
+    );
+  }
+
+  static TextStyle styleRegular24(context) {
+    return GoogleFonts.arimo(
+      fontSize: getResponsiveFontSize(context, fontSize: 24),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleRegular16(context) {
+    return GoogleFonts.arimo(
+      fontSize: getResponsiveFontSize(context, fontSize: 16),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleRegular14(context) {
+    return GoogleFonts.arimo(
+      fontSize: getResponsiveFontSize(context, fontSize: 14),
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle styleRegular18(context) {
+    return GoogleFonts.arimo(
+      fontSize: getResponsiveFontSize(context, fontSize: 18),
+      fontWeight: FontWeight.w600,
+    );
+  }
+}
+
+double getResponsiveFontSize(BuildContext context, {required double fontSize}) {
+  double scaleFactor = getScaleFactor(context);
+  double responsiveFontSize = fontSize * scaleFactor;
+  double lowerLimit = fontSize * 0.85;
+  double upperLimit = fontSize * 1.3;
+  return responsiveFontSize.clamp(lowerLimit, upperLimit);
+}
+
+double getScaleFactor(BuildContext context) {
+  double width = MediaQuery.sizeOf(context).width;
+  if (width <= 400) {
+    return 0.85;
+  } else if (width <= 800) {
+    return 1.0;
+  } else if (width <= 1200) {
+    return 1.15;
+  } else {
+    return 1.25;
+  }
+}
