88114fe76b4fcdf1ba1a61b4e2f2485884775bdf
Ali kamar <aliekamar123@gmail.com>
2025-09-30 01:44:57 +0300

    add app_colors and theme_data files for color and theme management

---
+++ lib/core/themes/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blackTextColor = Color(0xff333333);
+  static const greyTextColor = Color(0xff666666);
+  static const primaryColor = Color(0xff4CAF50);
+  static const whiteColor = Color(0xffFFFFFF);
+  static const orangeColor = Color(0xffFF9800);
+  static const scaffoldColor = Color(0xffF9F9F9);
+  static const blueColor = Color(0xff03A9F4);
+  static const borederColor = Color(0xffF3F3F5);
+  static const redColor = Color(0xffD4183D);
+}
---
+++ lib/core/themes/theme_data.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:waster/core/themes/app_colors.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData(
+    primaryColor: AppColors.primaryColor,
+    scaffoldBackgroundColor: AppColors.scaffoldColor,
+  );
+}
