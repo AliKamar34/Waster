a9e2613e1c9029ad2ad5dcbc39a699cf87c1357a
Ali kamar <aliekamar123@gmail.com>
2025-09-30 02:01:26 +0300

    Add Assets class for centralized asset management

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,42 @@
+class Assets {
+  static const String lottiePath = 'assets/lottie/';
+  static const String lottieSplash = '${lottiePath}splash.josn';
+
+  static const String imagesPath = 'assets/images/';
+  static const String logo = '${imagesPath}logo.svg';
+  static const String start = '${imagesPath}start.svg';
+
+  static const String iconsPath = 'assets/icons/';
+  static const String achievement = '${iconsPath}achievement.svg';
+  static const String camera = '${iconsPath}camera.svg';
+  static const String community = '${iconsPath}community.svg';
+  static const String cup = '${iconsPath}cup.svg';
+  static const String darkMode = '${iconsPath}dark_mode.svg';
+  static const String delete = '${iconsPath}delete.svg';
+  static const String delivered = '${iconsPath}delivered.svg';
+  static const String destination = '${iconsPath}destination.svg';
+  static const String edit = '${iconsPath}edit.svg';
+  static const String email = '${iconsPath}email.svg';
+  static const String expired = '${iconsPath}expired.svg';
+  static const String filters = '${iconsPath}filters.svg';
+  static const String heart = '${iconsPath}heart.svg';
+  static const String help = '${iconsPath}help.svg';
+  static const String home = '${iconsPath}home.svg';
+  static const String impact = '${iconsPath}impact.svg';
+  static const String language = '${iconsPath}language.svg';
+  static const String leaf = '${iconsPath}leaf.svg';
+  static const String location = '${iconsPath}location.svg';
+  static const String logOut = '${iconsPath}log_out.svg';
+  static const String more = '${iconsPath}more.svg';
+  static const String notification = '${iconsPath}notification.svg';
+  static const String package = '${iconsPath}package.svg';
+  static const String passwordEye = '${iconsPath}password_eye.svg';
+  static const String passwordLock = '${iconsPath}password_lock.svg';
+  static const String pending = '${iconsPath}pending.svg';
+  static const String phone = '${iconsPath}phone.svg';
+  static const String privacy = '${iconsPath}privacy.svg';
+  static const String profile = '${iconsPath}profile.svg';
+  static const String prowse = '${iconsPath}prowse.svg';
+  static const String timer = '${iconsPath}timer.svg';
+  static const String truck = '${iconsPath}truck.svg';
+}
