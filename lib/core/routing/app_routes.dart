30c7d4c2a83a57a4ec068638fd1b7fc5882f2601
Ali kamar <aliekamar123@gmail.com>
2025-10-02 20:38:44 +0300

    add app routes and router generation config to manage routing in the app

---
+++ lib/core/routing/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const String splachView = '/splachView';
+  static const String start = '/start';
+  static const String login = '/login';
+  static const String singUp = '/singUp';
+}
---
+++ lib/core/routing/router_generation_config.dart
@@ -0,0 +1,28 @@
+import 'package:go_router/go_router.dart';
+import 'package:waster/core/routing/app_routes.dart';
+import 'package:waster/features/auth/presentation/views/log_in_view.dart';
+import 'package:waster/features/start/views/splash_view.dart';
+import 'package:waster/features/start/views/start_view.dart';
+
+class RouterGenerationConfig {
+  static GoRouter goRoute = GoRouter(
+    initialLocation: AppRoutes.splachView,
+    routes: [
+      GoRoute(
+        path: AppRoutes.splachView,
+        name: AppRoutes.splachView,
+        builder: (context, state) => const SplashView(),
+      ),
+      GoRoute(
+        path: AppRoutes.start,
+        name: AppRoutes.start,
+        builder: (context, state) => const StartView(),
+      ),
+      GoRoute(
+        path: AppRoutes.login,
+        name: AppRoutes.login,
+        builder: (context, state) => const LogInView(),
+      ),
+    ],
+  );
+}
