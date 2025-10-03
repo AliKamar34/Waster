2d09bb53ab0bf798df3ebee9043c79797fe86d48
Ali kamar <aliekamar123@gmail.com>
2025-10-03 20:33:51 +0300

    update login button to navigate to main view and enhance text styles

---
+++ lib/features/home/presentation/views/main_view.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:waster/core/constants/assets.dart';
+import 'package:waster/core/routing/app_routes.dart';
+import 'package:waster/core/themes/app_colors.dart';
+import 'package:waster/core/themes/app_text_style.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  int currIndex = 0;
+  final List<Widget> pages = [
+    Container(color: Colors.amber),
+    Container(color: Colors.red),
+    const SizedBox.shrink(),
+    Container(color: Colors.blue),
+    Container(color: Colors.green),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        backgroundColor: AppColors.whiteColor,
+        currentIndex: currIndex,
+        selectedItemColor: AppColors.primaryColor,
+        type: BottomNavigationBarType.fixed,
+        selectedLabelStyle: AppTextStyle.styleMeduim14(context),
+        onTap: (value) {
+          if (value == 2) {
+            context.pushNamed(AppRoutes.login);
+          } else {
+            setState(() {
+              currIndex = value;
+            });
+          }
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              Assets.home,
+              colorFilter: ColorFilter.mode(
+                currIndex == 0
+                    ? AppColors.primaryColor
+                    : AppColors.blackTextColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              Assets.browse,
+              colorFilter: ColorFilter.mode(
+                currIndex == 1
+                    ? AppColors.primaryColor
+                    : AppColors.blackTextColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Browse',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.add, color: AppColors.blackTextColor),
+            label: 'Donate',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              Assets.impact,
+              colorFilter: ColorFilter.mode(
+                currIndex == 3
+                    ? AppColors.primaryColor
+                    : AppColors.blackTextColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Impact',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              Assets.profile,
+              colorFilter: ColorFilter.mode(
+                currIndex == 4
+                    ? AppColors.primaryColor
+                    : AppColors.blackTextColor,
+                BlendMode.srcIn,
+              ),
+            ),
+            label: 'Profile',
+          ),
+        ],
+      ),
+    );
+  }
+}
