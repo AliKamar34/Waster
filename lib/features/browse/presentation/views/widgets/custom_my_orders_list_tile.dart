73824ef4aed58c93b46a1ee37a1a97abf07cea56
Ali kamar <aliekamar123@gmail.com>
2025-10-06 02:39:02 +0300

    Add CustomMyOrdersListTile widget to display order details with an icon, title, and subtitle, enhancing the My Orders screen's UI.

---
+++ lib/features/browse/presentation/views/widgets/custom_my_orders_list_tile.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:waster/core/themes/app_colors.dart';
+import 'package:waster/core/themes/app_text_style.dart';
+
+class CustomMyOrdersListTile extends StatelessWidget {
+  const CustomMyOrdersListTile({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+    required this.iconColor,
+  });
+  final String title, subtitle, icon;
+  final Color iconColor;
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      contentPadding: EdgeInsets.zero,
+      leading: SvgPicture.asset(
+        icon,
+        colorFilter: ColorFilter.mode(iconColor, BlendMode.srcIn),
+      ),
+      title: Text(
+        title,
+        style: AppTextStyle.styleRegular16(
+          context,
+        ).copyWith(color: AppColors.greyTextColor),
+      ),
+      subtitle: Text(subtitle, style: AppTextStyle.styleRegular16(context)),
+    );
+  }
+}
