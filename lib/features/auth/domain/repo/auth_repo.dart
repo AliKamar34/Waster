6f4feb11a1782af0b5dfd2a83404f01e4af2a3bb
AliKamar34 <aliekamar123@gmail.com>
2025-10-27 02:27:55 -0700

    feat(auth): add auth repository interface

---
+++ lib/features/auth/domain/repo/auth_repo.dart
@@ -0,0 +1,19 @@
+import 'package:dartz/dartz.dart';
+import 'package:waster/core/errors/failure.dart';
+import 'package:waster/features/auth/domain/entity/auth_entity.dart';
+
+abstract class AuthRepo {
+  Future<Either<Failure, AuthEntity>> login({
+    required String email,
+    required String password,
+  });
+  Future<Either<Failure, AuthEntity>> register({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  });
+  Future<Either<Failure, AuthEntity>> refreshToken();
+  Future<Either<Failure, void>> revokeToken();
+}
