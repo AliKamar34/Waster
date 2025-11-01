ce28e37a0aad168aca8d795ab3edab8b7c2316b1
AliKamar34 <aliekamar123@gmail.com>
2025-11-01 03:26:17 -0700

    feat(auth): add remote data source for API calls

---
+++ lib/features/auth/data/datasource/auth_remote_date_source.dart
@@ -0,0 +1,108 @@
+import 'package:dio/dio.dart';
+import 'package:waster/core/errors/server_exception.dart';
+import 'package:waster/core/networking/api_end_points.dart';
+import 'package:waster/core/networking/dio_helper.dart';
+import 'package:waster/features/auth/data/models/auth_response_model.dart';
+
+abstract class AuthRemoteDateSource {
+  Future<AuthModel> login({required String email, required String password});
+  Future<AuthModel> register({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  });
+  Future<AuthModel> refreshToken({required String token});
+  Future<void> revokeToken({required String token});
+}
+
+class AuthRemoteDateSourceImpl implements AuthRemoteDateSource {
+  final DioHelper dioHelper;
+
+  const AuthRemoteDateSourceImpl({required this.dioHelper});
+  @override
+  Future<AuthModel> login({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      final response = await dioHelper.postRequest(
+        endPoint: ApiEndPoints.logIn,
+        data: {'email': email, 'password': password},
+      );
+      if (response.statusCode == 200) {
+        return AuthModel.fromJson(response.data);
+      } else {
+        throw ServerException(
+          message: 'Failed to login with $email and $password',
+        );
+      }
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Future<AuthModel> register({
+    required String firstName,
+    required String lastName,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    try {
+      final response = await dioHelper.postRequest(
+        endPoint: ApiEndPoints.register,
+        data: {
+          'firstName': firstName,
+          'lastName': lastName,
+          'email': email,
+          'password': password,
+          'confirmPassword': confirmPassword,
+        },
+      );
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        return AuthModel.fromJson(response.data);
+      } else {
+        throw ServerException(
+          message: 'Failed to login with $email and $password',
+        );
+      }
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Future<AuthModel> refreshToken({required String token}) async {
+    try {
+      final response = await dioHelper.postRequest(
+        endPoint: ApiEndPoints.refreshToken,
+        data: {'refreshToken': token},
+      );
+      if (response.statusCode == 200) {
+        return AuthModel.fromJson(response.data);
+      } else {
+        throw const ServerException(message: 'Failed to refresh token');
+      }
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Future<void> revokeToken({required String token}) async {
+    try {
+      final response = await dioHelper.postRequest(
+        endPoint: ApiEndPoints.refreshToken,
+        options: Options(headers: {'Authorization': 'Bearer $token'}),
+      );
+      if (response.statusCode != 200 || response.statusCode != 204) {
+        throw const ServerException(message: 'Failed to revoke token');
+      }
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+}
