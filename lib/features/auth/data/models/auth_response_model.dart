4f88bd7da105015531898cca703a0e37b0e60b6d
AliKamar34 <aliekamar123@gmail.com>
2025-11-01 03:25:27 -0700

    feat(auth): add auth model with fromJson implementation

---
+++ lib/features/auth/data/models/auth_response_model.dart
@@ -0,0 +1,26 @@
+import 'package:waster/features/auth/domain/entity/auth_entity.dart';
+
+class AuthModel extends AuthEntity {
+  AuthModel({
+    required super.token,
+    required super.refreshToken,
+    required super.email,
+    required super.userName,
+    required super.roles,
+    required super.expiresOn,
+    super.message,
+  });
+  factory AuthModel.fromJson(Map<String, dynamic> json) {
+    return AuthModel(
+      token: json['token'] as String,
+      refreshToken: json['refreshToken'] as String,
+      email: json['email'] as String,
+      userName: json['userName'] as String,
+      roles:
+          (json['roles'] as List<dynamic>?)?.map((e) => e as String).toList() ??
+          [],
+      expiresOn: DateTime.parse(json['expiresOn'] as String),
+      message: json['message'] as String?,
+    );
+  }
+}
--- lib/features/auth/domain/entity/auth_entity.dart
+++ lib/features/auth/domain/entity/auth_entity.dart
@@ -1,5 +1,18 @@
 class AuthEntity {
   final String token;
-
-  AuthEntity({required this.token});
+  final String refreshToken;
+  final String email;
+  final String userName;
+  final List<String> roles;
+  final DateTime expiresOn;
+  final String? message;
+  const AuthEntity({
+    required this.token,
+    required this.refreshToken,
+    required this.email,
+    required this.userName,
+    required this.roles,
+    required this.expiresOn,
+    this.message,
+  });
 }
