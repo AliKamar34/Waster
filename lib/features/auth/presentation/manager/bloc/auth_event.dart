3a3c6a7452ac788759c83624377de4b405b080e8
AliKamar34 <aliekamar123@gmail.com>
2025-11-01 22:33:00 -0700

    feat(auth): add auth bloc states and events

---
+++ lib/features/auth/presentation/manager/bloc/auth_event.dart
@@ -0,0 +1,51 @@
+part of 'auth_bloc.dart';
+
+sealed class AuthEvent extends Equatable {
+  const AuthEvent();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class LoginEvent extends AuthEvent {
+  final String email;
+  final String password;
+
+  const LoginEvent({required this.email, required this.password});
+
+  @override
+  List<Object> get props => [email, password];
+}
+
+final class RegisterEvent extends AuthEvent {
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String password;
+  final String confirmPassword;
+
+  const RegisterEvent({
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.password,
+    required this.confirmPassword,
+  });
+
+  @override
+  List<Object> get props => [
+    firstName,
+    lastName,
+    email,
+    password,
+    confirmPassword,
+  ];
+}
+
+final class RefreshTokenEvent extends AuthEvent {
+  const RefreshTokenEvent();
+}
+
+final class LogoutEvent extends AuthEvent {
+  const LogoutEvent();
+}
