42721f34a8bc0d099bb9412ffd90b7f8fe74d4fc
AliKamar34 <aliekamar123@gmail.com>
2025-11-01 22:34:17 -0700

    refactor(auth): move NoParams class to use_cases.dart and clean up use case files

--- lib/features/auth/domain/usecases/refresh_token_use_case.dart
+++ lib/features/auth/domain/usecases/refresh_token_use_case.dart
@@ -1,13 +1,8 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 import 'package:waster/core/errors/failure.dart';
+import 'package:waster/core/usecases/use_cases.dart';
 import 'package:waster/features/auth/domain/entity/auth_entity.dart';
 import 'package:waster/features/auth/domain/repo/auth_repo.dart';
-
-class NoParams extends Equatable {
-  @override
-  List<Object?> get props => [];
-}
 
 class RefreshTokenUseCase {
   final AuthRepo authRepo;
--- lib/features/auth/domain/usecases/revoke_token_use_case.dart
+++ lib/features/auth/domain/usecases/revoke_token_use_case.dart
@@ -1,12 +1,7 @@
 import 'package:dartz/dartz.dart';
-import 'package:equatable/equatable.dart';
 import 'package:waster/core/errors/failure.dart';
+import 'package:waster/core/usecases/use_cases.dart';
 import 'package:waster/features/auth/domain/repo/auth_repo.dart';
-
-class NoParams extends Equatable {
-  @override
-  List<Object?> get props => [];
-}
 
 class RevokeTokenUseCase {
   final AuthRepo authRepo;
