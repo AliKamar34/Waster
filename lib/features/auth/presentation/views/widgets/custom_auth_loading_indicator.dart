341ec4d9a1b13fe77909f44172cc5c38884c6bd2
AliKamar34 <aliekamar123@gmail.com>
2025-11-02 20:27:16 -0800

    feat(auth): add CustomAuthLoadingIndicator widget for improved loading state handling

---
+++ lib/features/auth/presentation/views/widgets/custom_auth_loading_indicator.dart
@@ -0,0 +1,54 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:waster/core/themes/app_colors.dart';
+
+class CustomAuthLoadinIndicator extends StatelessWidget {
+  final bool isLoading;
+  final Widget child;
+
+  const CustomAuthLoadinIndicator({
+    super.key,
+    required this.isLoading,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox.expand(
+      child: Stack(
+        children: [
+          child,
+
+          if (isLoading)
+            Positioned.fill(
+              child: Stack(
+                children: [
+                  BackdropFilter(
+                    filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                    child: Container(color: Colors.black.withAlpha(20)),
+                  ),
+
+                  Center(
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Theme.of(
+                          context,
+                        ).extension<AppColors>()!.whiteColor,
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: CircularProgressIndicator(
+                        color: Theme.of(
+                          context,
+                        ).extension<AppColors>()!.primaryColor,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
