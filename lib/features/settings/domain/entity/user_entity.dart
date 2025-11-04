87bb5290688ddd652fadec8809f587f2faab49b2
AliKamar34 <aliekamar123@gmail.com>
2025-11-03 23:04:34 -0800

    feat(settings): add user entity to domain layer

---
+++ lib/features/settings/domain/entity/user_entity.dart
@@ -0,0 +1,13 @@
+class UserEntity {
+  final String fullName;
+  final String email;
+  final String address;
+  final String phoneNumber;
+
+  const UserEntity({
+    required this.fullName,
+    required this.email,
+    required this.address,
+    required this.phoneNumber,
+  });
+}
