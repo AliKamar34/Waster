264f209ffb2bc848b10e4f8c5d2ec9cb88453039
AliKamar34 <aliekamar123@gmail.com>
2025-11-22 02:40:12 -0800

    create date formatter for displaying date

---
+++ lib/core/utils/date_formatter.dart
@@ -0,0 +1,49 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:waster/core/localization/locale_keys.g.dart';
+
+class DateFormatter {
+  static String formatRelative(DateTime dateTime, BuildContext context) {
+    final now = DateTime.now();
+    final diff = now.difference(dateTime);
+
+    if (diff.inSeconds < 60) {
+      return LocaleKeys.time_just_now.tr();
+    }
+
+    if (diff.inMinutes < 60) {
+      if (diff.inMinutes == 1) {
+        return LocaleKeys.time_minute_ago.tr();
+      }
+      return LocaleKeys.time_minutes_ago.tr(
+        namedArgs: {'count': diff.inMinutes.toString()},
+      );
+    }
+
+    if (diff.inHours < 24) {
+      if (diff.inHours == 1) {
+        return LocaleKeys.time_hour_ago.tr();
+      }
+      if (diff.inHours == 2) {
+        return LocaleKeys.time_two_hours_ago.tr();
+      }
+      return LocaleKeys.time_hours_ago.tr(
+        namedArgs: {'count': diff.inHours.toString()},
+      );
+    }
+
+    final yesterday = now.subtract(const Duration(days: 1));
+    if (dateTime.day == yesterday.day &&
+        dateTime.month == yesterday.month &&
+        dateTime.year == yesterday.year) {
+      return LocaleKeys.time_yesterday.tr();
+    }
+
+    final format = DateFormat(
+      'dd/MM/yy',
+      Localizations.localeOf(context).languageCode,
+    );
+
+    return format.format(dateTime);
+  }
+}
